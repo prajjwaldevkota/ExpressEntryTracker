5cba7f8592cf04fdbbb3aa4b318b0aa20089912e
prajjwaldevkota <68115199+prajjwaldevkota@users.noreply.github.com>
2025-04-19 07:29:56 -0400

    Updated to the latest available API

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "/api";
+export const BASE_URL = "https://65790b5c-can-ee-draws.karanjit-sagun01.workers.dev/api";
 export const NOC_URL = "https://e6276813-noc-canada.karanjit-sagun01.workers.dev";
